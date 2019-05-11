4ad9d9cbc6ecea52830439f46fa5ef75e60ba925
satya2221 <49893791+satya2221@users.noreply.github.com>
2019-05-11 12:40:59 +0700

    membetulkan ambil data
    silahkan dicoba dengan ngetest menggunakan main dulu mas

--- Laundry.cpp
+++ Laundry.cpp
@@ -19,17 +19,38 @@ void ambil_data(){
 	int j=0;
 	string carifile;
 	cout<<"Cari file yang akan diambil : ";cin>>carifile;
-ambil.open(carifile.c_str());
-	pelanggan data[banyakdata];
-	while(!ambil.eof()){
-		getline(cin,data[j].nama);
-		cin>>data[j].layanan;
-		cin>>data[j].lama;
-		cin>>data[j].berat;
-		cin>>data[j].harga;
-		j++;
-		banyakdata++;
+	ifstream ambil(carifile.c_str());
+	if(ambil.fail())
+	cout<<"error cuk";
+	int j=0;
+	for(int i=0;i<100;i++){
+		ambil>>data[i].nama;
+		if(ambil.eof())
+		break;
+		ambil>>data[i].layanan;
+		ambil>>data[i].lama;
+		ambil>>data[i].berat;
+		ambil>>data[i].harga;
 	}
+	
+	for (int i=0;i<3;i++){
+	cout<<data[i].nama<<endl;
+	cout<<data[i].layanan<<endl;
+	cout<<data[i].lama<<endl;
+	cout<<data[i].berat<<endl;
+	cout<<data[i].harga<<endl;
+	}
+// ambil.open(carifile.c_str());
+// 	pelanggan data[banyakdata];
+// 	while(!ambil.eof()){
+// 		getline(cin,data[j].nama);
+// 		cin>>data[j].layanan;
+// 		cin>>data[j].lama;
+// 		cin>>data[j].berat;
+// 		cin>>data[j].harga;
+// 		j++;
+// 		banyakdata++;
+	
 ambil.close();
 }
 
